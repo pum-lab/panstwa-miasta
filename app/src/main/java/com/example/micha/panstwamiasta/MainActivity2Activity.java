af97744f4f3e3ab1e2e5d645b906c7c99210d89c
dobrogowski <dobrogowski@gmail.com>
2015-04-25 16:27:56 +0200

    Test
    Test

--- app/src/main/java/com/example/micha/panstwamiasta/MainActivity2Activity.java
+++ app/src/main/java/com/example/micha/panstwamiasta/MainActivity2Activity.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity2Activity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_activity2);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
         return true;
     }
 
@@ -37,5 +37,3 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-
-onclick

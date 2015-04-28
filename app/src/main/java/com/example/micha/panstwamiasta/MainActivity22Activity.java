fb74539ff7dfbd2a8400b7d44f010a484d35d2ad
dobrogowski <dobrogowski@gmail.com>
2015-04-28 21:28:29 +0200

    Aktualizacja
    Wstępna obsługa BT

---
+++ app/src/main/java/com/example/micha/panstwamiasta/MainActivity22Activity.java
@@ -0,0 +1,72 @@
+package com.example.micha.panstwamiasta;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity22Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity22);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity22, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
+    // Register the BroadcastReceiver
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+    registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+
+    public void dalej(View view) {
+        Intent dalej = new Intent(this, MainActivity2Activity.class);
+        startActivity(dalej);
+    }
+
+
+
+}
--- app/src/main/java/com/example/micha/panstwamiasta/Wynik.java
+++ app/src/main/java/com/example/micha/panstwamiasta/Wynik.java
@@ -1,29 +1,26 @@
 package com.example.micha.panstwamiasta;
 
-import android.app.DownloadManager;
-import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Wynik extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wynik);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_wynik, menu);
         return true;
     }
 
@@ -42,30 +39,13 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void jedengracz(View view) {
-        Intent jedengracz = new Intent(this, MainActivity2Activity.class);
-        startActivity(jedengracz);
+    public void zestawieniewynikow(View view) {
+        Intent zestawieniewynikow = new Intent(this, Ranking.class);
+        startActivity(zestawieniewynikow);
     }
 
-    private final static int REQUEST_ENABLE_BT = 1;
-
-    public void dwochgraczy(View view) {
-
-        Intent dwochgraczy = new Intent(this, MainActivity22Activity.class);
-        startActivity(dwochgraczy);
-
-
-        BluetoothAdapter mBT = BluetoothAdapter.getDefaultAdapter();
-        if (mBT == null) {
-            // Device does not support Bluetooth
-        }
-
-        if (!mBT.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-
+    public void jeszczeraz(View view) {
+        Intent jeszczeraz = new Intent(this, MainActivity2Activity.class);
+        startActivity(jeszczeraz);
     }
 }
-

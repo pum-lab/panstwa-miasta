e8c99520b30b9f7c410eb858ac7cd6851beedc08
dobrogowski <dobrogowski@gmail.com>
2015-05-20 01:25:00 +0200

    BT
    BT

--- app/src/main/java/com/example/micha/panstwamiasta/MainActivity2Activity.java
+++ app/src/main/java/com/example/micha/panstwamiasta/MainActivity2Activity.java
@@ -1,29 +1,26 @@
 package com.example.micha.panstwamiasta;
 
-import android.app.DownloadManager;
-import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity2Activity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_activity2);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
         return true;
     }
 
@@ -42,30 +39,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void jedengracz(View view) {
-        Intent jedengracz = new Intent(this, MainActivity2Activity.class);
-        startActivity(jedengracz);
-    }
-
-    private final static int REQUEST_ENABLE_BT = 1;
-
-    public void dwochgraczy(View view) {
-
-        Intent dwochgraczy = new Intent(this, MainActivity22Activity.class);
-        startActivity(dwochgraczy);
-
-
-        BluetoothAdapter mBT = BluetoothAdapter.getDefaultAdapter();
-        if (mBT == null) {
-            // Device does not support Bluetooth
-        }
-
-        if (!mBT.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-
+    public void rozpocznijgre(View view) {
+        Intent rozpocznijgre = new Intent(this, Gra.class);
+        startActivity(rozpocznijgre);
     }
 }
-
